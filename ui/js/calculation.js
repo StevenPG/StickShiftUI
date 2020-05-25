bf9008ed04b47a8b31a61a2fa2e631cd518ebc15
StevenPG <steven.p.gantz@gmail.com>
2020-05-25 16:49:56 -0400

    calculator working for first gear

--- ui/js/calculation.js
+++ ui/js/calculation.js
@@ -28,20 +28,69 @@ function onRPMSlide(value) {
  * Tire Calculator helper function
  */
 
-function calculateTireDiameter(){
-    var tireWidth = parseInt(document.getElementById("tireWidth").value)
-    var aspectRatio = parseInt(document.getElementById("aspectRatio").value)
-    var wheelDiameter = parseInt(document.getElementById("wheelDiameter").value)
-    if( tireWidth.value != "" && aspectRatio.value != "" && wheelDiameter.value != "") {
-        var targetUpdateField = document.getElementById("tireDiameter")
-        var sectionWidth = tireWidth / 25.4
-        var sectionHeight = sectionWidth * (aspectRatio / 100)
-        var sectionHeightTwice = sectionHeight * 2
-        var result = sectionHeightTwice + wheelDiameter
-        targetUpdateField.value = result.toFixed(2)
-    }
+function calculateTireDiameter() {
+  var tireWidth = parseInt(document.getElementById("tireWidth").value);
+  var aspectRatio = parseInt(document.getElementById("aspectRatio").value);
+  var wheelDiameter = parseInt(document.getElementById("wheelDiameter").value);
+  var targetUpdateField = document.getElementById("tireDiameter");
+  var sectionWidth = tireWidth / 25.4;
+  var sectionHeight = sectionWidth * (aspectRatio / 100);
+  var sectionHeightTwice = sectionHeight * 2;
+  var result = sectionHeightTwice + wheelDiameter;
+  targetUpdateField.value = result.toFixed(2);
 }
 
 /**
  * End
  */
+
+/**
+ * Get the currently selected gear
+ */
+function selectedGearRatio() {
+  var first = document.getElementById("firstgear").value;
+  var second = document.getElementById("secondgear").value;
+  var third = document.getElementById("thirdgear").value;
+  var fourth = document.getElementById("fourthgear").value;
+  var fifth = document.getElementById("fifthgear").value;
+  var sixth = document.getElementById("sixthgear").value;
+  return first;
+}
+
+/**
+ * Calculation
+ *
+ * [ (Axle Ratio x Vehicle Speed x Transmission Ratio x 336.13) / Tire Diameter ] for RPM
+ **/
+function MPHChangeCalculation() {
+  var mph = document.getElementById("MPHSlider").value;
+  onMPHSlide(mph);
+
+  var finalDrive = document.getElementById("finaldrive").value;
+  var tireDiam = document.getElementById("tireDiameter").value;
+
+  var product = mph * finalDrive * selectedGearRatio() * 336.13;
+  var rpm = product / tireDiam;
+  document.getElementById("RPMSlider").value = rpm;
+  onRPMSlide(rpm.toFixed(0));
+}
+
+/**
+ * Calculation
+ *
+ * [ (RPM x Tire Diameter )/( Axle Ratio x Transmission Ratio x 336.13) ] for Vehicle Speed
+ **/
+function RPMChangeCalculation() {
+  var rpm = document.getElementById("RPMSlider").value;
+  onRPMSlide(rpm);
+
+  var finalDrive = document.getElementById("finaldrive").value;
+  var tireDiam = document.getElementById("tireDiameter").value;
+
+  var product = rpm * tireDiam
+  var distProduct = selectedGearRatio() * finalDrive * 336.13
+  var mph =product / distProduct
+
+  document.getElementById("MPHSlider").value = mph;
+  onMPHSlide(mph.toFixed(0));
+}
