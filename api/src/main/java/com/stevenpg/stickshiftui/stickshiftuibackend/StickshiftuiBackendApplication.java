821764f4d23ad03db305f6fecad2c6f6afedbc2b
Steven Gantz <steven.p.gantz@gmail.com>
2020-05-30 18:17:21 -0400

    add initial spring initializr stuff, no additional components

---
+++ api/src/main/java/com/stevenpg/stickshiftui/stickshiftuibackend/StickshiftuiBackendApplication.java
@@ -0,0 +1,13 @@
+package com.stevenpg.stickshiftui.stickshiftuibackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StickshiftuiBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StickshiftuiBackendApplication.class, args);
+	}
+
+}
