5d1cffb8eaffcd6d86643fa9e6f56f62e39ec401
StevenPG <steven.p.gantz@gmail.com>
2020-05-30 09:49:38 -0400

    remove added folder

--- deployment/deploy-updates.sh
+++ deployment/deploy-updates.sh
@@ -5,14 +5,14 @@ git clone https://github.com/StevenPG/StickShiftUI.git /deployment
 # compile
 cd /deployment && npm install
 mkdir -p /deployment/dist
-cp -rf /deployment/StickShiftUI/index.html /deployment/dist
-cp -rf /deployment/StickShiftUI/js /deployment/dist
-cp -rf /deployment/StickShiftUI/css /deployment/dist
-cp -rf /deployment/StickShiftUI/scss /deployment/dist
-cp -rf /deployment/StickShiftUI/vendor /deployment/dist
-cp -rf /deployment/StickShiftUI/404.html /deployment/dist
-cp -rf /deployment/StickShiftUI/aboutsite.html /deployment/dist
-cp -rf /deployment/StickShiftUI/usefulresources.html /deployment/dist
+cp -rf /deployment/index.html /deployment/dist
+cp -rf /deployment/js /deployment/dist
+cp -rf /deployment/css /deployment/dist
+cp -rf /deployment/scss /deployment/dist
+cp -rf /deployment/vendor /deployment/dist
+cp -rf /deployment/404.html /deployment/dist
+cp -rf /deployment/aboutsite.html /deployment/dist
+cp -rf /deployment/usefulresources.html /deployment/dist
 # delete files in nginx folder
 rm -rf /var/www/html/*
 # move results of compilation into nginx folder
