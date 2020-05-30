3e9dcdcc29878428b6115f3d873b0ec503fc3da4
StevenPG <steven.p.gantz@gmail.com>
2020-05-30 16:58:25 -0400

    add nearly finalized ansible and docker components

--- deployment/setup/get_roles.sh
+++ deployment/setup/get_roles.sh
@@ -1,1 +1,4 @@
 ansible-galaxy install geerlingguy.docker
+
+# Make sure you have the right version of python(3)-pip here, could be python-pip
+apt install -y python3-pip python-dockerpty python3-docker
