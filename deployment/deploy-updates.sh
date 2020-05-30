d0d9cf97209bbda778ce92dcb901344bb1b9a4d4
StevenPG <steven.p.gantz@gmail.com>
2020-05-30 09:51:37 -0400

    add correct folder

--- deployment/deploy-updates.sh
+++ deployment/deploy-updates.sh
@@ -5,14 +5,14 @@ git clone https://github.com/StevenPG/StickShiftUI.git /deployment
 # compile
 cd /deployment && npm install
 mkdir -p /deployment/dist
-cp -rf /deployment/index.html /deployment/dist
-cp -rf /deployment/js /deployment/dist
-cp -rf /deployment/css /deployment/dist
-cp -rf /deployment/scss /deployment/dist
-cp -rf /deployment/vendor /deployment/dist
-cp -rf /deployment/404.html /deployment/dist
-cp -rf /deployment/aboutsite.html /deployment/dist
-cp -rf /deployment/usefulresources.html /deployment/dist
+cp -rf /deployment/ui/index.html /deployment/dist
+cp -rf /deployment/ui/js /deployment/dist
+cp -rf /deployment/ui/css /deployment/dist
+cp -rf /deployment/ui/scss /deployment/dist
+cp -rf /deployment/ui/vendor /deployment/dist
+cp -rf /deployment/ui/404.html /deployment/dist
+cp -rf /deployment/ui/aboutsite.html /deployment/dist
+cp -rf /deployment/ui/usefulresources.html /deployment/dist
 # delete files in nginx folder
 rm -rf /var/www/html/*
 # move results of compilation into nginx folder
