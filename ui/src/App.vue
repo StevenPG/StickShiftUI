9c995e57284c951fe51bba64c3b5fb7900f750ac
StevenPG <steven.p.gantz@gmail.com>
2020-05-30 14:06:19 -0400

    remove vue componentst that are unused

--- ui/src/App.vue
+++ ui/src/App.vue
@@ -1,19 +1,6 @@
 <template>
-  <div id="app">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <div id="app"></div>
 </template>
-
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
 
 <style>
 #app {
